c7b7f7dd6c0c74cdf4800afbf6feb24ac8fa1c18
Penguinfami <christine.spyphone.zhao@gmail.com>
2024-10-06 00:59:18 -0400

    feat: image display

---
+++ src/components/ViewPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row class="flex flex-column justify-center align-center">
+    <h1 class="h3 header">Your QuickiePixie Masterpiece</h1>
+    <v-row>
+        <img src=""></img>
+    </v-row>
+  </v-row>
+</template>
+
+<script setup>
+  //
+</script>
+
+<script>
+const 
+</script>
---
+++ src/pages/image/[id].vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row class="flex flex-column justify-center align-center">
+    <h1 class="h3 header">Your QuickiePixie Masterpiece</h1>
+    <v-row>
+        <img :src="url" type="image/png"></img>
+        <a class="" :href="url" download="quickiepixie.png">Download as PNG</a>
+    </v-row>
+  </v-row>
+</template>
+
+<script setup>
+  //
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+console.log(route);
+
+const imageId = route.params.id;
+
+console.log(imageId)
+
+const BASE_URL = `${process.env.VUE_APP_BACKEND_URL}/image`;
+
+const url = `${BASE_URL}/${imageId}`;
+</script>
+
+<script>
+</script>
